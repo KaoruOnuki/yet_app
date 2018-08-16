d86be1431608dd656137ae731a1507115ca5b410
KaoruOnuki <kaoru.onuki@gmail.com>
2018-08-16 13:05:12 +0900

    close #9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :correct_user?, only: [:show, :edit, :update]
   def new
     @user = User.new
   end
@@ -13,12 +14,30 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    redirect_to new_session_path if @user != current_user
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_picture, :profile_picture_cache)
+  end
+
+  def correct_user?
+    @user = User.find(params[:id])
+    redirect_to new_session_path if @user != current_user
   end
 end
