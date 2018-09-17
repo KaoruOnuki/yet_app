9276d63e6fb72982bce51bb3527f521bd37a7a9a
KaoruOnuki <kaoru.onuki@gmail.com>
2018-09-17 20:05:45 +0900

    change image scales

--- app/uploaders/profilepicture_uploader.rb
+++ app/uploaders/profilepicture_uploader.rb
@@ -2,7 +2,7 @@ class ProfilepictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
   # storage :fog
-  process :resize_to_limit => [1080, nil]
+  process :resize_to_limit => [500, nil]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
